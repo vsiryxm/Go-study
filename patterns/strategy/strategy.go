5712663dc043aa737d93cfbeedbe40ce05c4d84a
Simon <vsiryxm@qq.com>
2019-09-15 23:03:20 +0800

    策略者模式

---
+++ patterns/strategy/strategy.go
@@ -0,0 +1,29 @@
+package strategy
+
+//策略者模式
+//定义：策略模式可以让更换对象的内脏，而装饰者模式可以更换对象的外表。我们可以定义算法，封装它们，动态地切换它们。
+
+type Operator interface {
+	Apply(int, int) int
+}
+
+type Operation struct {
+	Operator Operator
+}
+
+func (this *Operation) Operate(l, r int) int {
+	return this.Operator.Apply(l, r)
+}
+
+//开始动态地往类里添加方法逻辑
+type Addition struct{}
+
+func (Addition) Apply(lval, rval int) int {
+	return lval + rval
+}
+
+type Subition struct{} //我加了个减法方法
+
+func (Subition) Apply(lval, rval int) int { //相当于可以随时重写这个方法
+	return lval - rval
+}
---
+++ patterns/strategy/strategy_test.go
@@ -0,0 +1,22 @@
+package strategy
+
+import (
+	"fmt"
+	"testing"
+)
+
+//策略者模式
+//定义：策略模式可以让更换对象的内脏，而装饰者模式可以更换对象的外表。我们可以定义算法，封装它们，动态地切换它们。
+
+func TestStrategy(test *testing.T) {
+	fmt.Println("装载加法方法：")
+	operation := Operation{Addition{}} //往里塞个加方法，后面可以定义减方法或其他方法，也是这么个塞法，灵活机动
+	a := 1985
+	b := 1024
+	res := operation.Operate(a, b)
+	fmt.Printf("%d + %d = %d\n", a, b, res)
+	fmt.Println("装载减法方法：")
+	operation = Operation{Subition{}}
+	res = operation.Operate(a, b)
+	fmt.Printf("%d - %d = %d\n", a, b, res)
+}
