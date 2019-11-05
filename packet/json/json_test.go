382662d534ca66ad2b3fd0126f2e5c879647d096
Simon <vsiryxm@qq.com>
2019-11-05 18:54:53 +0800

    json包学习

---
+++ packet/json/json_test.go
@@ -0,0 +1,14 @@
+package json
+
+import (
+	//"fmt"
+	"testing"
+)
+
+func TestJson(test *testing.T) {
+	//JsonToStruct()
+	StructToJson()
+	//ReadJsonFile()  //读json文件
+	//WriteJsonFile() //写json文件
+	JsonX()
+}
