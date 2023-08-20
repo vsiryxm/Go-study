3f5e99ed5694a60e3bd25738f64fe43815918e02
blockman <vsiryxm@qq.com>
2023-08-20 15:54:47 +0800

    增加Unicode编码表

--- syntax/byte_rune.go
+++ syntax/byte_rune.go
@@ -4,15 +4,14 @@ import "fmt"
 
 func main() {
 
-	var num1 byte = 255 //byte等价于uint8
+	var num1 byte = 255 //byte等价于uint8，也就是uint8的别名
 	fmt.Printf("num1 = %d\n", num1)
 
 	var sex rune = '男'
 	fmt.Printf("字符 '%c' 的Unicode代码点是：%s\n", sex, "U+7537")
 
-	//Unicode编码表中文查询：http://unicode-table.com/cn/
+	// Unicode编码表中文查询：http://unicode-table.com/cn/
 	var me rune = '\u6211' //我  支持\uFFFF、\U7FFFFFFF、\xFF格式
 	fmt.Println(me)
-
 
 }
